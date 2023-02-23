a1cab8da7c132dd05885fb67f3a26d4ae3989b01
marcowijaya <106919629+marcowijaya@users.noreply.github.com>
2023-02-23 18:01:09 +0000

    import new hydrogen cg model

--- src/fastga/models/weight/mass_breakdown/b_propulsion/sum.py
+++ src/fastga/models/weight/mass_breakdown/b_propulsion/sum.py
@@ -25,11 +25,51 @@ from .constants import (
 ) #additional code
 from ..constants import SUBMODEL_PROPULSION_MASS
 
-
 @oad.RegisterSubmodel(
     SUBMODEL_PROPULSION_MASS, "fastga.submodel.weight.mass.propulsion.legacy.fuel"
 )
 class PropulsionWeight(om.Group):
+    """Computes mass of propulsion system."""
+
+    def initialize(self):
+        self.options.declare("propulsion_id", default="", types=str)
+
+    def setup(self):
+        propulsion_option = {"propulsion_id": self.options["propulsion_id"]}
+        self.add_subsystem(
+            "engine_weight",
+            oad.RegisterSubmodel.get_submodel(
+                SUBMODEL_INSTALLED_ENGINE_MASS, options=propulsion_option
+            ),
+            promotes=["*"],
+        )
+        self.add_subsystem(
+            "unusable_fuel",
+            oad.RegisterSubmodel.get_submodel(
+                SUBMODEL_UNUSABLE_FUEL_MASS, options=propulsion_option
+            ),
+            promotes=["*"],
+        )
+        self.add_subsystem(
+            "fuel_lines_weight",
+            oad.RegisterSubmodel.get_submodel(SUBMODEL_FUEL_SYSTEM_MASS),
+            promotes=["*"],
+        )
+
+        weight_sum = om.AddSubtractComp()
+        weight_sum.add_equation(
+            "data:weight:propulsion:mass",
+            ["data:weight:propulsion:engine:mass", "data:weight:propulsion:fuel_lines:mass"],
+            units="kg",
+            desc="Mass of the propulsion system",
+        ) #additional code
+
+        self.add_subsystem("propulsion_weight_sum", weight_sum, promotes=["*"])
+
+@oad.RegisterSubmodel(
+    SUBMODEL_PROPULSION_MASS, "fastga.submodel.weight.mass.propulsion.hydrogen.fuel"
+)
+class PropulsionWeightHydrogen(om.Group):
     """Computes mass of propulsion system."""
 
     def initialize(self):
